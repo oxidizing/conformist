e2cc417d3e0180bcc86dacdbbfe14aeaa0096599
Josef Erben <josef@erben.io>
2022-01-11 14:24:49 +0100

    Release 0.8.0

--- default.nix
+++ default.nix
@@ -1,6 +1,17 @@
 with import <nixpkgs> { };
 
 mkShell {
-  buildInputs = [ zlib.dev zlib.out zlib zlib.all gmp gmp.dev pkgconfig openssl libev libevdev ];
+  buildInputs = [
+    zlib.dev
+    zlib.out
+    zlib
+    zlib.all
+    gmp
+    gmp.dev
+    pkgconfig
+    openssl
+    libev
+    libevdev
+  ];
   shellHook = "eval $(opam env)";
 }
